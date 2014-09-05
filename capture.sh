5f0fa2d85c1585d2711a9fa5e80b1723c577e705
hagiyat <hagiyat@gmail.com>
2014-09-05 14:12:18 +0900

    新しく開いたウィンドウにreattach-to-user-namespaceがあたらない問題を解決

--- capture.sh
+++ capture.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 tmux capture-pane -S -2000\; show-buffer > /tmp/tmuxcap_`date +"%Y%m%d%H%M%S"`.txt
-tmux split-window -h "ls -t /tmp/tmuxcap_*.txt | head -n 1 | xargs cat | vim +2000 -Rc 'set ft=zsh' -"
+tmux new-window -n "--history"
+tmux set-option -t "--history" default-command "reattach-to-user-namespace -l zsh"
+tmux send-keys -t:"--history" "ls -t /tmp/tmuxcap_*.txt | head -n 1 | xargs cat | vim +2000 -Rc 'set ft=zsh' - && exit" C-m
