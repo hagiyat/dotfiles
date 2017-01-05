4c8bc30ac92a3277f0ff521e3eddbbc4709c11bd
hagiyat <hagiyat@gmail.com>
2017-01-05 16:06:16 +0900

    Remove: tmux capture

--- bin/capture.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-tmux capture-pane -S -5000\; show-buffer > /tmp/tmuxcap_`date +"%Y%m%d%H%M%S"`.txt
-tmux new-window -n "--history"
-tmux set-option -t "--history" default-command "reattach-to-user-namespace -l zsh"
-tmux send-keys -t:"--history" "ls -t /tmp/tmuxcap_*.txt | head -n 1 | xargs cat | vim +5000 -Rc 'set ft=zsh' - && exit" C-m
