cc62c4ab51d4c838f6ed9d94f6340fba4badd459
hagiyat <hagiyat@gmail.com>
2017-05-17 14:32:08 +0900

    Add xremap configuration

---
+++ xremap.rb
@@ -0,0 +1,9 @@
+window class_only: 'slack' do
+  remap 'Ctrl-n', to: 'Down'
+  remap 'Ctrl-p', to: 'Up'
+  remap 'Ctrl-g', to: 'Escape'
+end
+
+window class_only: %w(google-chrome crx_mpognobbkildjkofajifpdfhcoklimli) do
+  remap 'Ctrl-g', to: 'Escape'
+end
