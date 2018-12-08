bed349985f5c2637691159f369fb81297b039722
hagiyat <hagiyat@gmail.com>
2018-12-08 22:55:38 +0900

    remove fish links

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import os
 
-directories = [
-    ".config/fish"
-]
 links = [
     (".config/nvim", "nvim"),
-    # (".config/vimfx", "vimfx"),
     # (".config/fish/config.fish", "fish/config.fish"),
     # (".config/fish/conf.d", "fish/conf.d"),
     (".gitconfig", "gitconfig"),
-    # (".hammerspoon", "hammerspoon"),
     (".myclirc", "myclirc"),
     # (".spacemacs", "spacemacs"),
     (".tmux.conf", "tmux.conf"),
@@ -24,14 +18,6 @@ links = [
 
 home = os.environ["HOME"]
 dotfiles = os.path.abspath(os.path.curdir)
-
-for directory in directories:
-    path = "/".join([home, directory])
-    if os.path.isdir(path):
-        print("[exists] %s" % path)
-    else:
-        os.makedirs(path)
-        print("[created] %s" % path)
 
 for (dest, source) in links:
     destpath = "/".join([home, dest])
