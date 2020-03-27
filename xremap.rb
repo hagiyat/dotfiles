636465b6a89cc639cb39ab41d8fc6e7cc29cb5dc
hagiyat <hagiyat@gmail.com>
2020-03-27 16:28:19 +0900

    remove xremap

--- xremap.rb
+++
@@ -1,9 +0,0 @@
-window class_only: %w(slack) do
-  remap 'Ctrl-n', to: 'Down'
-  remap 'Ctrl-p', to: 'Up'
-  remap 'Ctrl-g', to: 'Escape'
-end
-
-window class_only: %w(Navigator google-chrome crx_mpognobbkildjkofajifpdfhcoklimli) do
-  remap 'Ctrl-g', to: 'Escape'
-end
