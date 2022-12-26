192f95c6f6b2c249f49a00ec2357a34d363bbd8f
hagiyat <hagiyat@gmail.com>
2022-12-26 23:39:07 +0900

    add hop

--- nvim/lua/modules/base.lua
+++ nvim/lua/modules/base.lua
@@ -2,13 +2,69 @@ return {
   setup = function(use)
     use {
       "lambdalisue/gina.vim",
-      event = { "BufEnter" },
+      cmd = { "Gina" },
       opt = true
     }
 
     use {
       "nvim-tree/nvim-web-devicons",
+      module = { "nvim-web-devicons" },
       opt = true
+    }
+
+    use {
+      "phaazon/hop.nvim",
+      requires = "which-key",
+      event = "BufReadPost",
+      branch = 'v2',
+      config = function()
+        local hop = require("hop")
+        hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+
+        local directions = require("hop.hint").HintDirection
+        local wk = require("which-key")
+        wk.register({
+          ["f"] = {
+            function()
+              hop.hint_char1({
+                direction = directions.AFTER_CURSOR,
+                current_line_only = true
+              })
+            end,
+            "hop forward"
+          },
+          ["F"] = {
+            function()
+              hop.hint_char1({
+                direction = directions.BEFORE_CURSOR,
+                current_line_only = true
+              })
+            end,
+            "hop backward"
+          },
+          ["t"] = {
+            function()
+              hop.hint_char1({
+                direction = directions.AFTER_CURSOR,
+                current_line_only = true,
+                hint_offset = -1
+              })
+            end,
+            "hop forward -1"
+          },
+          ["T"] = {
+            function()
+              hop.hint_char1({
+                direction = directions.BEFORE_CURSOR,
+                current_line_only = true,
+                hint_offset = 1
+              })
+            end,
+            "hop backward +1"
+          },
+        },
+        { remap = true, mode = "n" } )
+      end
     }
   end
 }
