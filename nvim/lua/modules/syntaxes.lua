f2974259db23e968184dca0fbd80cfe3519a63bc
hagiyat <hagiyat@gmail.com>
2022-12-30 12:17:29 +0900

    add: tree-sitter

--- nvim/lua/modules/syntaxes.lua
+++ nvim/lua/modules/syntaxes.lua
@@ -1,4 +1,37 @@
 return {
   setup = function(use)
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = function()
+        local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+        ts_update()
+      end,
+      config = function()
+        require("nvim-treesitter.configs").setup({
+          highlight = {
+            enable = true
+          },
+          indent = {
+            enable = true
+          },
+          ensure_installed = {
+            "bash",
+            "css",
+            "dockerfile",
+            "html",
+            "json",
+            "lua",
+            "make",
+            "markdown",
+            "python",
+            "ruby",
+            "toml",
+            "tsx",
+            "typescript",
+            "yaml",
+          },
+        })
+      end
+    }
   end
 }
