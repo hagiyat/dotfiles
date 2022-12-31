e30525fe7cf041241a34c30ef556050c8afcc5f3
hagiyat <hagiyat@gmail.com>
2022-12-31 17:40:07 +0900

    add configuration for sumneko_lua

--- nvim/lua/modules/lsp.lua
+++ nvim/lua/modules/lsp.lua
@@ -19,40 +19,64 @@ return {
           }
         })
         local mason_lspconfig = require('mason-lspconfig')
-        local nvim_lsp = require("lspconfig")
-        local wk = require("which-key")
+        local lspconfig = require("lspconfig")
 
-        mason_lspconfig.setup_handlers({ function(server_name)
-          local opts = {}
-          opts.on_attach = function(_, bufnr)
-            local bufopts = { silent = true, buffer = bufnr }
-            wk.register({
-              c = {
-                name = "+lsp",
-                d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "definition" },
-                i = { "<Cmd>lua vim.lsp.buf.implementation()<CR>", "implementation" },
-                h = { "<Cmd>lua vim.lsp.buf.signature_help()<CR>", "signature help" },
-                D = { "<Cmd>lua vim.lsp.buf.references()<CR>", "references" },
-                t = { "<Cmd>lua vim.lsp.buf.type_definition()<CR>", "type definition" },
-                e = { "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "show line diagnostics" },
-                f = { "<Cmd>lua vim.lsp.buf.format()<CR>", "format" },
-                r = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "rename" },
-              }
-            }, { prefix = "<space>", noremap = true, mode = "n" })
-            wk.register({
-              g = {
-                name = "+lsp",
-                [","] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "diagnostic prev" },
-                ["."] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "diagnostic next" }
-              }
+        mason_lspconfig.setup_handlers({
+          function(server_name)
+            local opts= {}
+            opts.on_attach = function(_, bufnr)
+              local wk = require("which-key")
+              wk.register({
+                c = {
+                  name = "+lsp",
+                  d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "definition" },
+                  i = { "<Cmd>lua vim.lsp.buf.implementation()<CR>", "implementation" },
+                  h = { "<Cmd>lua vim.lsp.buf.signature_help()<CR>", "signature help" },
+                  D = { "<Cmd>lua vim.lsp.buf.references()<CR>", "references" },
+                  t = { "<Cmd>lua vim.lsp.buf.type_definition()<CR>", "type definition" },
+                  e = { "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "show line diagnostics" },
+                  f = { "<Cmd>lua vim.lsp.buf.format()<CR>", "format" },
+                  r = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "rename" },
+                }
+              }, { buffer = bufnr, prefix = "<space>", noremap = true, mode = "n" })
+              wk.register({
+                g = {
+                  name = "+lsp",
+                  [","] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "diagnostic prev" },
+                  ["."] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "diagnostic next" }
+                }
 
-            }, { noremap = true, mode = "n" })
-            wk.register({
-              ["K"] = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "lsp hover" },
-            }, { noremap = true, mode = "n" } )
+              }, { buffer = bufnr, noremap = true, mode = "n" })
+              wk.register({
+                ["K"] = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "lsp hover" },
+              }, { buffer = bufnr, noremap = true, mode = "n" } )
+            end
+            -- opts.capabilities = require('cmp_nvim_lsp').update_capabilities(
+            --   vim.lsp.protocol.make_client_capabilities()
+            -- )
+
+            if server_name == "sumneko_lua" then
+              local sumneko_opts = {
+                settings = {
+                  Lua = {
+                    diagnostics = {
+                      globals = { "vim" },
+                    },
+                    workspace = {
+                      library = {
+                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                      },
+                    },
+                  },
+                },
+              }
+              opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+            end
+
+            lspconfig[server_name].setup(opts)
           end
-          nvim_lsp[server_name].setup(opts)
-        end })
+        })
       end
     }
   end
