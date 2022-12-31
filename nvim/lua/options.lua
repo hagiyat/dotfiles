e8e79dc5987130d3b9fdbb5d4dbca173918ae3c5
hagiyat <hagiyat@gmail.com>
2023-01-01 00:43:08 +0900

    fix options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,22 +1,22 @@
-vim.o.termguicolors = true
-vim.o.backgroud = "dark"
-vim.o.number = true
-vim.o.novisualbell = true
-vim.o.noshowcmd = true
-vim.o.cursorline = true
-vim.o.clipboard = "unnamedplus"
-vim.o.fileencodings = "utf-8,iso-2022-jp,cp932"
-vim.o.autoindent = true
-vim.o.showmatch = true
-vim.o.matchtime = 3
-vim.o.smartindent = true
-vim.o.smarttab = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.hlsearch = true
-vim.o.list = true
-vim.o.wrap = true
-vim.o.textwidth = 0
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.number = true
+vim.opt.visualbell = false
+vim.opt.showcmd = false
+vim.opt.cursorline = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.fileencodings = "utf-8,iso-2022-jp,cp932"
+vim.opt.autoindent = true
+vim.opt.showmatch = true
+vim.opt.matchtime = 3
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.list = true
+vim.opt.wrap = true
+vim.opt.textwidth = 0
 vim.opt.listchars = {
   tab = "▸-",
   trail = "↔",
@@ -33,24 +33,24 @@ vim.opt.fillchars = {
   foldsep = "│",
   foldclose = "▸",
 }
-vim.o.infercase = true
-vim.o.switchbuf = "useopen"
-vim.o.backspace = "indent,eol,start"
+vim.opt.infercase = true
+vim.opt.switchbuf = "useopen"
+vim.opt.backspace = "indent,eol,start"
 
 -- 日本語入力のときの文字化け対応
--- vim.o.ttimeout = true
--- vim.o.ttimeoutlen = 50
+-- vim.opt.ttimeout = true
+-- vim.opt.ttimeoutlen = 50
 
 -- Swapファイル,Backupファイルなど全て無効化する
-vim.o.nowritebackup = true
-vim.o.nobackup = true
-vim.o.noswapfile = true
+vim.opt.writebackup = false
+vim.opt.backup = false
+vim.opt.swapfile = false
 
 -- 水平分割は下に開く
-vim.o.splitbelow = true
+vim.opt.splitbelow = true
 -- 垂直分割は右に開く
-vim.o.splitright = true
+vim.opt.splitright = true
 
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
