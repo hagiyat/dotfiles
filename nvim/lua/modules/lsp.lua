1592cc1d83ca07993b8e017992ce6338bb8396e1
hagiyat <hagiyat@gmail.com>
2023-01-01 17:28:04 +0900

    add null-ls

--- nvim/lua/modules/lsp.lua
+++ nvim/lua/modules/lsp.lua
@@ -6,27 +6,34 @@ return {
         { "neovim/nvim-lspconfig" },
         { "williamboman/mason-lspconfig.nvim" },
         { "folke/which-key.nvim" },
+        { "jose-elias-alvarez/null-ls.nvim" },
       },
       event = { "BufReadPre" },
-      wants = { "cmp-nvim-lsp" },
+      wants = {
+        "cmp-nvim-lsp",
+        "plenary.nvim",
+      },
       config = function()
-        require('mason').setup({
+        require("mason").setup {
           ui = {
             icons = {
               package_installed = "✓",
               package_pending = "➜",
-              package_uninstalled = "✗"
-            }
-          }
-        })
-        local mason_lspconfig = require('mason-lspconfig')
-        local lspconfig = require("lspconfig")
+              package_uninstalled = "✗",
+            },
+          },
+        }
 
-        mason_lspconfig.setup_handlers({
+        local mason_lspconfig = require("mason-lspconfig")
+        local lspconfig = require("lspconfig")
+        local wk = require("which-key")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local null_ls = require("null-ls")
+
+        mason_lspconfig.setup_handlers {
           function(server_name)
-            local opts= {}
+            local opts = {}
             opts.on_attach = function(_, bufnr)
-              local wk = require("which-key")
               wk.register({
                 c = {
                   name = "+lsp",
@@ -38,22 +45,61 @@ return {
                   e = { vim.lsp.diagnostic.show_line_diagnostics, "show line diagnostics" },
                   f = { vim.lsp.buf.format, "format" },
                   r = { vim.lsp.buf.rename, "rename" },
-                }
+                },
               }, { buffer = bufnr, prefix = "<space>", noremap = true, mode = "n" })
               wk.register({
                 g = {
                   name = "+lsp",
                   [","] = { vim.diagnostic.goto_prev, "diagnostic prev" },
-                  ["."] = { vim.diagnostic.goto_next, "diagnostic next" }
-                }
-
+                  ["."] = { vim.diagnostic.goto_next, "diagnostic next" },
+                },
               }, { buffer = bufnr, noremap = true, mode = "n" })
               wk.register({
                 ["K"] = { vim.lsp.buf.hover, "lsp hover" },
-              }, { buffer = bufnr, noremap = true, mode = "n" } )
+              }, { buffer = bufnr, noremap = true, mode = "n" })
             end
 
-            opts.capabilities = require('cmp_nvim_lsp').default_capabilities()
+            opts.capabilities = cmp_nvim_lsp.default_capabilities()
+
+            null_ls.setup {
+              capabilities = opts.capabilities,
+              sources = {
+                null_ls.builtins.formatting.stylua.with {
+                  condition = function(utils)
+                    return utils.root_has_file { ".stylua.toml" }
+                  end,
+                },
+                null_ls.builtins.diagnostics.luacheck.with {
+                  extra_args = { "--global vim" },
+                },
+
+                null_ls.builtins.formatting.prettier.with {
+                  condition = function(utils)
+                    return utils.root_has_file { ".prettierrc.json" }
+                  end,
+                },
+                null_ls.builtins.diagnostics.eslint.with {
+                  condition = function(utils)
+                    return utils.root_has_file { ".eslintrc.yml" }
+                  end,
+                },
+
+                null_ls.builtins.formatting.rubocop.with {
+                  prefer_local = "bundle_bin",
+                  condition = function(utils)
+                    return utils.root_has_file { ".rubocop.yml" }
+                  end,
+                },
+                null_ls.builtins.diagnostics.rubocop.with {
+                  prefer_local = "bundle_bin",
+                  condition = function(utils)
+                    return utils.root_has_file { ".rubocop.yml" }
+                  end,
+                },
+
+                null_ls.builtins.diagnostics.yamllint,
+              },
+            }
 
             if server_name == "sumneko_lua" then
               local sumneko_opts = {
@@ -75,9 +121,9 @@ return {
             end
 
             lspconfig[server_name].setup(opts)
-          end
-        })
-      end
+          end,
+        }
+      end,
     }
-  end
+  end,
 }
