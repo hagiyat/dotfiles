d5d221c5f0cee02aee39816aabc85529a7340bd3
hagiyat <hagiyat@gmail.com>
2023-01-03 01:02:05 +0900

    add init_local

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,13 @@
---    ___  ___ ___ _  __(_)_ _   --
---   / _ \/ -_) _ \ |/ / /  ` \  --
---  /_//_/\__/\___/___/_/_/_/_/  --
---                               --
-
 require("options")
 require("autocmds")
+
+-- exsample init_local.lua
+-- echo 'vim.g.ruby_host_prog = vim.env.HOME .. "/path/to/neovim-ruby-host"' > init_local.lua
+local local_lua_path = vim.env.XDG_CONFIG_HOME .. "/nvim/lua/init_local.lua"
+local stat = vim.loop.fs_stat(local_lua_path)
+if stat and stat.type == "file" then
+  require("init_local")
+end
+
 require("keymaps")
 require("plugins")
