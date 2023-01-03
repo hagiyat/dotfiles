f28827fc358fc7c0932cb5a2f3878ad6454419f1
hagiyat <hagiyat@gmail.com>
2023-01-03 17:55:54 +0900

    add devicons to lsp completion

--- nvim/lua/modules/completion.lua
+++ nvim/lua/modules/completion.lua
@@ -8,12 +8,18 @@ return {
         { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
         { "hrsh7th/vim-vsnip", event = "InsertEnter" },
         { "hrsh7th/vim-vsnip-integ", event = "InsertEnter" },
+        { "onsails/lspkind.nvim", event = "InsertEnter" },
         { "hrsh7th/cmp-path", event = "CmdlineEnter" },
         { "hrsh7th/cmp-buffer", event = "InsertEnter" },
         { "hrsh7th/cmp-cmdline", event = "CmdlineEnter" },
+        { "hrsh7th/cmp-cmdline", event = "CmdlineEnter" },
+        { "kyazdani42/nvim-web-devicons" },
       },
+      wants = { "lspkind.nvim" },
       config = function()
         local cmp = require("cmp")
+        local lspkind = require("lspkind")
+        local devicons = require("nvim-web-devicons")
         cmp.setup {
           snippet = {
             expand = function(args)
@@ -37,6 +43,19 @@ return {
           experimental = {
             ghost_text = true,
           },
+          formatting = {
+            format = function(entry, vim_item)
+              if vim.tbl_contains({ "path" }, entry.source.name) then
+                local icon, hl_group = devicons.get_icon(entry:get_completion_item().label)
+                if icon then
+                  vim_item.kind = icon
+                  vim_item.kind_hl_group = hl_group
+                  return vim_item
+                end
+              end
+              return lspkind.cmp_format({ with_text = true })(entry, vim_item)
+            end
+          }
         }
         cmp.setup.cmdline("/", {
           mapping = cmp.mapping.preset.cmdline(),
