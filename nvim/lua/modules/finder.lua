9d475c8904fd91eefd8bee559aa67c98e8ebd9c4
hagiyat <hagiyat@gmail.com>
2023-01-21 13:39:57 +0900

    add: telescope-fzf-native

--- nvim/lua/modules/finder.lua
+++ nvim/lua/modules/finder.lua
@@ -5,8 +5,16 @@ return {
       tag = "0.1.0",
       -- cmd = { "Telescope" },
       event = { "VimEnter" },
+      requires = {
+        {
+          "nvim-telescope/telescope-fzf-native.nvim",
+          run = "make",
+          opt = true,
+        },
+      },
       wants = {
         "plenary.nvim",
+        "telescope-fzf-native.nvim",
         "nvim-web-devicons",
         "which-key.nvim",
         "trouble.nvim",
@@ -15,6 +23,7 @@ return {
         local telescope = require("telescope")
         local actions = require("telescope.actions")
         local trouble = require("trouble.providers.telescope")
+
         telescope.setup {
           defaults = {
             layout_strategy = "center",
@@ -38,7 +47,17 @@ return {
               -- sorting_strategy = "descending",
             },
           },
+          extensions = {
+            fzf = {
+              fuzzy = true, -- false will only do exact matching
+              override_generic_sorter = true, -- override the generic sorter
+              override_file_sorter = true, -- override the file sorter
+              case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+              -- the default case_mode is "smart_case"
+            },
+          },
         }
+        telescope.load_extension("fzf")
 
         local picker = require("telescope.builtin")
         -- local extensions = telescope.extensions
