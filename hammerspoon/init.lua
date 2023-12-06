5b4ce2899b379e416740ee1d6c5f835ae8ca9982
hagiyat <hagiyat@gmail.com>
2023-12-06 10:54:42 +0900

    fix keybinds

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,19 +2,9 @@ local window                      = require 'hs.window'
 local hotkey                      = require 'hs.hotkey'
 local application                 = require 'hs.application'
 
--- Window Switcher
--- https://mac-ra.com/window-kirikae-hammerspoon/
-window.animationDuration          = 0
-local switcher                    = window.switcher.new()
-switcher.ui.showSelectedTitle     = false
-switcher.ui.showSelectedThumbnail = false
-switcher.ui.backgroundColor       = { 0.3, 0.3, 0.3, 0.5 }
-hotkey.bind({ 'cmd', 'shift' }, 'j', 'Next window', function() switcher:next() end, nil, function() switcher:next() end)
-hotkey.bind({ 'cmd', 'shift' }, 'k', 'Prev window', function() switcher:previous() end, nil, function() switcher:previous() end)
+local RecursiveBinder = hs.loadSpoon("RecursiveBinder") -- Load the spoon
 
-local RecursiveBinder = hs.loadSpoon("RecursiveBinder")  -- Load the spoon
-
-RecursiveBinder.escapeKey = {{}, 'escape'}  -- Press escape to abort
+RecursiveBinder.escapeKey = { {}, 'escape' }            -- Press escape to abort
 
 -- singleKey is a convenience function provided in the Spoon
 local singleKey = spoon.RecursiveBinder.singleKey
@@ -25,7 +15,25 @@ local applicationKeyMap = {
   [singleKey('t', 'WezTerm')] = function() application.launchOrFocus("WezTerm") end,
   [singleKey('f', 'Finder')] = function() application.launchOrFocus("Finder") end,
 }
-hs.hotkey.bind({'option'}, 'a', RecursiveBinder.recursiveBind(applicationKeyMap))
+hotkey.bind({ 'option' }, 'a', RecursiveBinder.recursiveBind(applicationKeyMap))
+
+
+-- Window Switcher
+-- https://github.com/oniatsu/HotSwitch-HS
+-- 2023/11現在、スクリーンセーバーが立ち上がると選択不能になってしまう
+-- local hotswitchHs = require("hotswitch-hs/hotswitch-hs")
+-- hotkey.bind({ "alt" }, "tab", hotswitchHs.openOrClose) -- Set a keybind you like to open HotSwitch-HS panel.
+-- hotkey.bind({}, "f13", hotswitchHs.openOrClose)
+
+-- https://mac-ra.com/window-kirikae-hammerspoon/
+window.animationDuration          = 0
+local switcher                    = window.switcher.new()
+switcher.ui.showSelectedTitle     = false
+switcher.ui.showSelectedThumbnail = false
+switcher.ui.backgroundColor       = { 0.3, 0.3, 0.3, 0.5 }
+hotkey.bind({ 'alt' }, 'tab', 'Next window', function() switcher:next() end, nil, function() switcher:next() end)
+hotkey.bind({ 'alt', 'shift' }, 'tab', 'Prev window', function() switcher:previous() end, nil, function() switcher:previous() end)
+
 
 -- Window Management
 -- https://martinlwx.github.io/en/how-to-manage-windows-using-hammerspoon/
@@ -41,4 +49,4 @@ local windowKeyMap = {
   [singleKey('j', 'next window')] = function() switcher:next() end,
   [singleKey('k', 'previous window')] = function() switcher:previous() end,
 }
-hs.hotkey.bind({'option'}, 'w', RecursiveBinder.recursiveBind(windowKeyMap))
+hs.hotkey.bind({ 'option' }, 'w', RecursiveBinder.recursiveBind(windowKeyMap))
