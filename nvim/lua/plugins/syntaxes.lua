fe132738cc2188d91212e02e0a9c734e8c6b6b69
hagiyat <hagiyat@gmail.com>
2024-11-22 19:09:11 +0900

    add treesitter-textobjects

--- nvim/lua/plugins/syntaxes.lua
+++ nvim/lua/plugins/syntaxes.lua
@@ -13,6 +13,7 @@ return {
       },
       "RRethy/nvim-treesitter-endwise",
       "RRethy/nvim-treesitter-textsubjects",
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
       require("nvim-treesitter.configs").setup {
@@ -55,6 +56,50 @@ return {
             ["i;"] = "textsubjects-container-inner",
           },
         },
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+              ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+              ["]c"] = { query = "@class.outer", desc = "Next class start" },
+              ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+              ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+              ["]a"] = { query = "@assignment.rhs", desc = "Assignment rhs" },
+              ["]b"] = { query = "@block.outer", desc = "Next block start" },
+              ["]s"] = { query = "@statement.outer", desc = "Next statement start" },
+            },
+            goto_next_end = {
+              ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+              ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+              ["]C"] = { query = "@class.outer", desc = "Next class end" },
+              ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+              ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+              ["]B"] = { query = "@block.outer", desc = "Next block end" },
+              ["]S"] = { query = "@statement.outer", desc = "Next statement end" },
+            },
+            goto_previous_start = {
+              ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+              ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+              ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+              ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+              ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+              ["[b"] = { query = "@block.outer", desc = "Prev block start" },
+              ["[a"] = { query = "@assignment.lhs", desc = "Assignment lhs" },
+              ["[s"] = { query = "@statement.outer", desc = "Prev statement start" },
+            },
+            goto_previous_end = {
+              ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+              ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+              ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+              ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+              ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+              ["[B"] = { query = "@block.outer", desc = "Prev block end" },
+              ["[S"] = { query = "@statement.outer", desc = "Prev statement end" },
+            },
+          },
+        }
       }
     end,
   },
